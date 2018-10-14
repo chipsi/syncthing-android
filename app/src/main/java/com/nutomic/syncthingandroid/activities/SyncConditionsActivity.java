9064dc62a4d6e490497c67b39160005cc0138359
Catfriend1 <Catfriend1@users.noreply.github.com>
2018-10-14 11:23:33 +0200

    Read per-folder sync conditions from prefs

--- app/src/main/java/com/nutomic/syncthingandroid/activities/SyncConditionsActivity.java
+++ app/src/main/java/com/nutomic/syncthingandroid/activities/SyncConditionsActivity.java
@@ -8,6 +8,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.v7.widget.SwitchCompat;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -42,13 +43,33 @@ import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 public class SyncConditionsActivity extends SyncthingActivity
         implements SyncthingService.OnServiceStateChangeListener {
 
+    private static final String TAG = "SyncConditionsActivity";
+
     private static final String EXTRA_OBJECT_PREFIX_AND_ID =
             "com.nutomic.syncthingandroid.activities.SyncConditionsActivity.OBJECT_PREFIX_AND_ID";
 
     private static final String EXTRA_OBJECT_READABLE_NAME =
             "com.nutomic.syncthingandroid.activities.SyncConditionsActivity.OBJECT_READABLE_NAME";
 
+    // UI elements
+    private SwitchCompat mSyncOnWifi;
+    private SwitchCompat mSyncOnWhitelistedWifi;
     private ViewGroup mWifiSsidContainer;
+    private SwitchCompat mSyncOnMeteredWifi;
+    private SwitchCompat mSyncOnMobileData;
+
+    /**
+     * Shared preferences names for custom per-folder settings.
+     */
+    private String mObjectPrefixAndId;
+    private String mPrefSyncOnWifi;
+    private String mPrefSyncOnWhitelistedWifi;
+    private String mPrefSelectedWhitelistSsid;
+    private String mPrefSyncOnMeteredWifi;
+    private String mPrefSyncOnMobileData;
+
+    private String mObjectReadableName;
+    private boolean mFolderNeedsToUpdate = false;
 
     @Inject
     SharedPreferences mPreferences;
@@ -65,16 +86,66 @@ public class SyncConditionsActivity extends SyncthingActivity
         super.onCreate(savedInstanceState);
         ((SyncthingApp) getApplication()).component().inject(this);
 
-        setContentView(R.layout.activity_sync_conditions);
+        Intent intent = getIntent();
+        if (!intent.hasExtra(EXTRA_OBJECT_PREFIX_AND_ID) ||
+                !intent.hasExtra(EXTRA_OBJECT_READABLE_NAME)) {
+            Log.e(TAG, "onCreate extra missing");
+            return;
+        }
+        mObjectReadableName = intent.getStringExtra(EXTRA_OBJECT_READABLE_NAME);
 
-        // Populate wifiSsidContainer.
+        // Display content and get views.
+        setContentView(R.layout.activity_sync_conditions);
+        mSyncOnWifi = findViewById(R.id.sync_on_wifi_title);
+        mSyncOnWhitelistedWifi = findViewById(R.id.sync_on_whitelisted_wifi_title);
         mWifiSsidContainer = findViewById(R.id.wifiSsidContainer);
-        mWifiSsidContainer.removeAllViews();
+        mSyncOnMeteredWifi = findViewById(R.id.sync_on_metered_wifi_title);
+        mSyncOnMobileData = findViewById(R.id.sync_on_mobile_data_title);
+
+        // Generate shared preferences names.
+        mObjectPrefixAndId = intent.getStringExtra(EXTRA_OBJECT_PREFIX_AND_ID);
+        mPrefSyncOnWifi = mObjectPrefixAndId + "_" + Constants.PREF_RUN_ON_WIFI;
+        mPrefSyncOnWhitelistedWifi = mObjectPrefixAndId + "_" + "use_wifi_whitelist";
+        mPrefSelectedWhitelistSsid = mObjectPrefixAndId + "_" + Constants.PREF_WIFI_SSID_WHITELIST;
+        mPrefSyncOnMeteredWifi = mObjectPrefixAndId + "_" + Constants.PREF_RUN_ON_METERED_WIFI;
+        mPrefSyncOnMobileData = mObjectPrefixAndId + "_" + Constants.PREF_RUN_ON_MOBILE_DATA;
+
+        /**
+         * Load global run conditions.
+         */
+        Boolean globalRunOnWifiEnabled = mPreferences.getBoolean(Constants.PREF_RUN_ON_WIFI, false);
+        Boolean globalWhitelistEnabled = !mPreferences.getStringSet(Constants.PREF_WIFI_SSID_WHITELIST, new HashSet<>())
+                .isEmpty();
         Set<String> globalWhitelistedSsid = mPreferences.getStringSet(Constants.PREF_WIFI_SSID_WHITELIST, new HashSet<>());
+        Boolean globalRunOnMeteredWifiEnabled = mPreferences.getBoolean(Constants.PREF_RUN_ON_METERED_WIFI, false);
+        Boolean globalRunOnMobileDataEnabled = mPreferences.getBoolean(Constants.PREF_RUN_ON_MOBILE_DATA, false);
+
+        /**
+         * Load custom folder preferences. If unset, use global setting as default.
+         */
+        mSyncOnWifi.setChecked(mPreferences.getBoolean(mPrefSyncOnWifi, globalRunOnWifiEnabled));
+        mSyncOnWifi.setEnabled(globalRunOnWifiEnabled);
+
+        mSyncOnWhitelistedWifi.setChecked(mPreferences.getBoolean(mPrefSyncOnWhitelistedWifi, globalWhitelistEnabled));
+        mSyncOnWhitelistedWifi.setEnabled(globalWhitelistEnabled);
+
+        mSyncOnMeteredWifi.setChecked(mPreferences.getBoolean(mPrefSyncOnMeteredWifi, globalRunOnMeteredWifiEnabled));
+        mSyncOnMeteredWifi.setEnabled(globalRunOnMeteredWifiEnabled);
+
+        mSyncOnMobileData.setChecked(mPreferences.getBoolean(mPrefSyncOnMobileData, globalRunOnMobileDataEnabled));
+        mSyncOnMobileData.setEnabled(globalRunOnMobileDataEnabled);
+
+        // Read selected WiFi Ssid whitelist items.
+        Set<String> selectedWhitelistedSsid = mPreferences.getStringSet(mPrefSelectedWhitelistSsid, new HashSet<>());
+        // Removes any network that is no longer part of the global WiFi Ssid whitelist.
+        selectedWhitelistedSsid.retainAll(globalWhitelistedSsid);
+
+        // Populate WiFi Ssid whitelist.
+        mWifiSsidContainer.removeAllViews();
         // from JavaDoc: Note that you must not modify the set instance returned by this call.
         // therefore required to make a defensive copy of the elements
         globalWhitelistedSsid = new HashSet<>(globalWhitelistedSsid);
-        if (globalWhitelistedSsid.isEmpty()) {
+        if (!globalWhitelistEnabled) {
             // Add empty WiFi Ssid ListView.
             int height = (int) TypedValue.applyDimension(COMPLEX_UNIT_DIP, 48, getResources().getDisplayMetrics());
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(WRAP_CONTENT, height);
@@ -88,33 +159,19 @@ public class SyncConditionsActivity extends SyncthingActivity
             mWifiSsidContainer.addView(emptyView, params);
         } else {
             for (String wifiSsid : globalWhitelistedSsid) {
-                // Strip quotes.
+                // Strip quotes and add  WiFi Ssid to view.
                 wifiSsid = wifiSsid.replaceFirst("^\"", "").replaceFirst("\"$", "");
-                // Add WiFi Ssid to list.
                 LayoutInflater layoutInflater = getLayoutInflater();
                 layoutInflater.inflate(R.layout.item_wifi_ssid_form, mWifiSsidContainer);
                 SwitchCompat wifiSsidView = (SwitchCompat) mWifiSsidContainer.getChildAt(mWifiSsidContainer.getChildCount()-1);
                 wifiSsidView.setOnCheckedChangeListener(null);
-                // ToDo wifiSsidView.setChecked(mFolder.getDevice(device.deviceID) != null);
+                wifiSsidView.setChecked(selectedWhitelistedSsid.contains(wifiSsid));
                 wifiSsidView.setText(wifiSsid);
                 wifiSsidView.setTag(wifiSsid);
                 wifiSsidView.setOnCheckedChangeListener(mCheckedListener);
             }
         }
-
-        /*
-        if (getIntent().hasExtra(EXTRA_INITIAL_DIRECTORY)) {
-            displayFolder(new File(getIntent().getStringExtra(EXTRA_INITIAL_DIRECTORY)));
-        } else {
-            displayRoot();
-        }
-
-        Boolean prefUseRoot = PreferenceManager.getDefaultSharedPreferences(this).getBoolean(Constants.PREF_USE_ROOT, false);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && !prefUseRoot) {
-            Toast.makeText(this, R.string.kitkat_external_storage_warning, Toast.LENGTH_LONG)
-                    .show();
-        }
-        */
+        mWifiSsidContainer.setEnabled(!globalWhitelistEnabled);
     }
 
     private final CompoundButton.OnCheckedChangeListener mCheckedListener =
@@ -123,7 +180,7 @@ public class SyncConditionsActivity extends SyncthingActivity
         public void onCheckedChanged(CompoundButton view, boolean isChecked) {
             switch (view.getId()) {
                 default:
-                    // ToDo
+                    mFolderNeedsToUpdate = true;
                     break;
             }
         }
