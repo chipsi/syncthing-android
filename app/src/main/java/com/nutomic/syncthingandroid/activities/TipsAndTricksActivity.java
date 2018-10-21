5543e6cad0e33d00e8ee2cd6df31ac36d0151323
Catfriend1 <Catfriend1@users.noreply.github.com>
2018-10-21 19:33:33 +0200

    Add onClick listener

--- app/src/main/java/com/nutomic/syncthingandroid/activities/TipsAndTricksActivity.java
+++ app/src/main/java/com/nutomic/syncthingandroid/activities/TipsAndTricksActivity.java
@@ -3,10 +3,12 @@ package com.nutomic.syncthingandroid.activities;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-// import android.util.Log;
+import android.util.Log;
+import android.view.View;
 
 import com.nutomic.syncthingandroid.R;
 import com.nutomic.syncthingandroid.views.TipListAdapter;
+import com.nutomic.syncthingandroid.views.TipListAdapter.ItemClickListener;
 
 import java.util.ArrayList;
 
@@ -15,7 +17,7 @@ import java.util.ArrayList;
  */
 public class TipsAndTricksActivity extends SyncthingActivity {
 
-    // private static final String TAG = "TipsAndTricksActivity";
+    private static final String TAG = "TipsAndTricksActivity";
 
     private RecyclerView mRecyclerView;
     private TipListAdapter mTipListAdapter;
@@ -35,6 +37,19 @@ public class TipsAndTricksActivity extends SyncthingActivity {
         for (int i = 1; i < 100; i++) {
             mTipListAdapter.add("Title " + i, "Text " + i + "Today I want to tell you a story about Syncthing 1.\nToday I want to tell you a story about Syncthing 2.\nToday I want to tell you a story about Syncthing 3. Today I want to tell you a story about Syncthing 4. Today I want to tell you a story about Syncthing 5. Today I want to tell you a story about Syncthing 6. Today I want to tell you a story about Syncthing 7.\nTest.");
         }
+        mTipListAdapter.setOnClickListener(
+            new ItemClickListener() {
+                @Override
+                public void onItemClick(View view, String itemTitle, String itemText) {
+                    Log.v(TAG, "User clicked item with title \'" + itemTitle + "\'");
+                    /**
+                     * Future improvement:
+                     * Collapse texts to the first three lines and open a DialogFragment
+                     * if the user clicks an item from the tip list.
+                     */
+                }
+            }
+        );
         mRecyclerView.setAdapter(mTipListAdapter);
     }
 
