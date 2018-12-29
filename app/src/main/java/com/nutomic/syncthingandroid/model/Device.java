b8d463df0bdeca4fb612a257926c86ea2ecaab8d
Catfriend1 <Catfriend1@users.noreply.github.com>
2018-12-29 22:02:10 +0100

    Add checkDeviceID to model/Device.java
    and verify device ID's entered by the user before writing them to the config.

--- app/src/main/java/com/nutomic/syncthingandroid/model/Device.java
+++ app/src/main/java/com/nutomic/syncthingandroid/model/Device.java
@@ -1,7 +1,14 @@
 package com.nutomic.syncthingandroid.model;
 
 import android.text.TextUtils;
+// import android.util.Log;
 
+import com.google.common.io.BaseEncoding;
+
+import com.nutomic.syncthingandroid.util.Luhn;
+
+import java.lang.System;
+import java.util.Arrays;
 import java.util.List;
 
 public class Device {
@@ -34,5 +41,74 @@ public class Device {
         return (TextUtils.isEmpty(name))
                 ? (TextUtils.isEmpty(deviceID) ? "" : deviceID.substring(0, 7))
                 : name;
+    }
+
+    /**
+     * Returns if a syncthing device ID is correctly formatted.
+     */
+    public Boolean checkDeviceID() {
+        /**
+         * See https://github.com/syncthing/syncthing/blob/master/lib/protocol/deviceid.go
+         * how syncthing validates device IDs.
+         * Old dirty way to check was: return deviceID.matches("^([A-Z0-9]{7}-){7}[A-Z0-9]{7}$");
+         */
+        String deviceID = new String(this.deviceID);
+
+        // Trim "="
+        deviceID = deviceID.replaceAll("=", "");
+
+        // Convert to upper case.
+        deviceID = deviceID.toUpperCase();
+
+        // untypeoify
+        deviceID = deviceID.replaceAll("1", "I");
+        deviceID = deviceID.replaceAll("0", "O");
+        deviceID = deviceID.replaceAll("8", "B");
+
+        // unchunkify
+        deviceID = deviceID.replaceAll("-", "");
+        deviceID = deviceID.replaceAll(" ", "");
+
+        // Check length.
+        switch(deviceID.length()) {
+            case 0:
+                // Log.w(TAG, "checkDeviceID: Empty device ID.");
+                return false;
+            case 56:
+                // unluhnify(deviceID)
+                byte bytesIn[] = deviceID.getBytes();
+                byte res[] = new byte[52];
+                for (int i = 0; i < 4; i++) {
+                    byte[] p = Arrays.copyOfRange(bytesIn, i*(13+1), (i+1)*(13+1)-1);
+                    System.arraycopy(p, 0, res, i*13, 13);
+
+                    // Generate check digit.
+                    Luhn luhn = new Luhn();
+                    String checkRune = luhn.generate(p);
+                    // Log.v(TAG, "checkDeviceID: luhn.generate(" + new String(p) + ") returned (" + checkRune + ")");
+                    if (checkRune == null) {
+                        // Log.w(TAG, "checkDeviceID: deviceID=(" + deviceID + "): invalid character");
+                        return false;
+                    }
+                    if (!deviceID.substring((i+1)*14-1, (i+1)*14-1+1).equals(checkRune)) {
+                        // Log.w(TAG, "checkDeviceID: deviceID=(" + deviceID + "): check digit incorrect");
+                        return false;
+                    }
+                }
+                deviceID = new String(res);
+                // Log.v(TAG, "isDeviceIdValid: unluhnify(deviceID)=" + deviceID);
+                // Fall-Through
+            case 52:
+                try {
+                    BaseEncoding.base32().decode(deviceID + "====");
+                    return true;
+                } catch (IllegalArgumentException e) {
+                    // Log.w(TAG, "checkDeviceID: deviceID=(" + deviceID + "): invalid character, base32 decode failed");
+                    return false;
+                }
+            default:
+                // Log.w(TAG, "checkDeviceID: Incorrect length (" + deviceID + ")");
+                return false;
+        }
     }
 }
