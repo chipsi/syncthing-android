f74f1f3146f97cb67c3968e47eba576dd89dccf7
Catfriend1 <Catfriend1@users.noreply.github.com>
2019-01-01 18:38:25 +0100

    Add purpose and example to helper script

--- postbuild_rename_apk.cmd
+++ postbuild_rename_apk.cmd
@@ -1,4 +1,11 @@
 @echo off
+REM 
+REM Purpose:
+REM 	Rename and move built APK's to match this style:
+REM 		[APPLICATION_ID]_v[VERSION_NAME]_[COMMIT_SHORT_HASH].apk
+REM 	Example:
+REM 		com.github.catfriend1.syncthingandroid_v1.0.0.1_7d59e75.apk
+REM 
 title %~nx0
 setlocal enabledelayedexpansion
 cls
@@ -25,12 +32,13 @@ FOR /F "tokens=1" %%A IN ('git rev-parse --short --verify HEAD 2^>NUL:') DO SET 
 echo [INFO] commit="%COMMIT_SHORT_HASH%"
 REM 
 REM Rename APK to be ready for upload to the GitHub release page.
-call :renIfExist %SCRIPT_PATH%app\build\outputs\apk\debug\app-debug.apk %APPLICATION_ID%_%VERSION_NAME%_%COMMIT_SHORT_HASH%.apk
-call :renIfExist %SCRIPT_PATH%app\build\outputs\apk\release\app-release-unsigned.apk %APPLICATION_ID%_%VERSION_NAME%_%COMMIT_SHORT_HASH%.apk
+SET APK_NEW_FILENAME=%APPLICATION_ID%_v%VERSION_NAME%_%COMMIT_SHORT_HASH%.apk
+call :renIfExist %SCRIPT_PATH%app\build\outputs\apk\debug\app-debug.apk %APK_NEW_FILENAME%
+call :renIfExist %SCRIPT_PATH%app\build\outputs\apk\release\app-release-unsigned.apk %APK_NEW_FILENAME%
 echo [INFO] APK file rename step complete.
 REM 
 REM Copy debug APK to temporary storage location if the storage is available.
-IF EXIST %TEMP_OUTPUT_FOLDER% copy /y %SCRIPT_PATH%app\build\outputs\apk\debug\%APPLICATION_ID%_%VERSION_NAME%_%COMMIT_SHORT_HASH%.apk %TEMP_OUTPUT_FOLDER% 2> NUL:
+IF EXIST %TEMP_OUTPUT_FOLDER% copy /y %SCRIPT_PATH%app\build\outputs\apk\debug\%APK_NEW_FILENAME% %TEMP_OUTPUT_FOLDER% 2> NUL:
 REM 
 echo [INFO] End of Script.
 timeout 3
