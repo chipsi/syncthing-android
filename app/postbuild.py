96be5bde3620d862e88a78aa47e1109210e29d7f
Catfriend1 <Catfriend1@users.noreply.github.com>
2019-01-06 20:59:15 +0100

    WIP

--- app/postbuild.py
+++ app/postbuild.py
@@ -4,6 +4,8 @@ import os.path
 import sys
 import subprocess
 import platform
+import codecs
+import re
 #
 # Script Compatibility:
 # - Python 2.7.15
@@ -54,6 +56,37 @@ def calcAndPrintCertHash(apk_fullfn, apk_build_type):
             "dQAnHXvlh80yJgrQUCo6LAg4294=": "Google Play"
         }
         print('[INFO] Built ' + apk_build_type + ' APK for ' + release_types.get(result_hash, "INVALID_CHANNEL") + ' (signing certificate hash: ' + result_hash + ')')
+
+    if (platform.system() == 'Windows'):
+            keytool_bin = which("keytool")
+            if not keytool_bin:
+                keytool_bin = os.environ.get('ProgramFiles') + os.path.sep + 'Android' + os.path.sep + 'Android Studio' + os.path.sep + 'jre' + os.path.sep + 'bin' + os.path.sep + 'keytool.exe'
+            try:
+                shell_result = subprocess.check_output(keytool_bin + ' -list -printcert -jarfile "' + apk_fullfn + '"')
+            except Exception as e:
+                print('[WARN] Failed to exec keytool: ' + str(e));
+                return None
+
+            try:
+                result_array = codecs.decode(shell_result, 'cp1252').splitlines()
+                for result_line in result_array:
+                    if result_line:
+                        result_line = result_line.strip()
+                        if 'SHA1: ' in result_line:
+                            result_hex = result_line.replace('SHA1: ', '')
+                            result_hex_cleaned = re.sub('[^A-Fa-f0-9]+', '', result_hex)
+                            result_hash = codecs.encode(codecs.decode(result_hex_cleaned, 'hex'), 'base64').decode('utf-8')
+                            result_hash = result_hash.strip('\n')
+            except Exception as e:
+                print('[WARN] Failed to parse keytool result: ' + str(e));
+                return None
+
+            release_types = {
+                "2ScaPj41giu4vFh+Y7Q0GJTqwbA=": "GitHub",
+                "nyupq9aU0x6yK8RHaPra5GbTqQY=": "F-Droid",
+                "dQAnHXvlh80yJgrQUCo6LAg4294=": "Google Play"
+            }
+            print('[INFO] Built ' + apk_build_type + ' APK for ' + release_types.get(result_hash, "INVALID_CHANNEL") + ' (signing certificate hash: ' + result_hash + ')')
 
     return None
 
@@ -106,7 +139,7 @@ if platform.system() not in SUPPORTED_PYTHON_PLATFORMS:
          ', '.join(SUPPORTED_PYTHON_PLATFORMS))
 print ('')
 
-# Build FullFN of "app-debug.apk".
+# Build FullFNs.
 current_dir = os.path.dirname(os.path.realpath(__file__))
 enable_push_to_device = os.path.realpath(os.path.join(current_dir, "..", "#enable_push_to_device"))
 debug_apk = os.path.realpath(os.path.join(current_dir, 'build', 'outputs', 'apk', 'debug', 'app-debug.apk'))
