117304778edf149f76d6c3f71df341363f55d61f
Catfriend1 <Catfriend1@users.noreply.github.com>
2019-03-28 23:14:52 +0100

    Fix lint: RequiresApi(21) for SyncTriggerJobService

--- app/src/main/java/com/nutomic/syncthingandroid/service/SyncTriggerJobService.java
+++ app/src/main/java/com/nutomic/syncthingandroid/service/SyncTriggerJobService.java
@@ -4,6 +4,7 @@ import android.app.job.JobParameters;
 import android.app.job.JobService;
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.RequiresApi;
 import android.support.v4.content.LocalBroadcastManager;
 // import android.util.Log;
 
@@ -15,6 +16,7 @@ import com.nutomic.syncthingandroid.util.JobUtils;
  * SyncTriggerJobService to be scheduled by the JobScheduler.
  * See {@link JobUtils#scheduleSyncTriggerServiceJob} for more details.
  */
+@RequiresApi(21)
 public class SyncTriggerJobService extends JobService {
     private static final String TAG = "SyncTriggerJobService";
 
