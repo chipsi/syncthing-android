4c745c8ef954c912bd991f728c24d9d03ee7d9e3
Catfriend1 <Catfriend1@users.noreply.github.com>
2019-04-13 16:26:25 +0200

    Revert DeviceActivity AlertDialog.Builder(this)

--- app/src/main/java/com/nutomic/syncthingandroid/activities/DeviceActivity.java
+++ app/src/main/java/com/nutomic/syncthingandroid/activities/DeviceActivity.java
@@ -465,7 +465,7 @@ public class DeviceActivity extends SyncthingActivity {
     }
 
     private Dialog createDeleteDialog(){
-        return new AlertDialog.Builder(this)
+        return new android.app.AlertDialog.Builder(this)
                 .setMessage(R.string.remove_device_confirm)
                 .setPositiveButton(android.R.string.yes, (dialogInterface, i) -> {
                     mConfig.removeDevice(getApi(), mDevice.deviceID);
