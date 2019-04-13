d6161f98011cc15822898fa9b0a11cb3b687cbd3
Catfriend1 <Catfriend1@users.noreply.github.com>
2019-04-13 14:33:00 +0200

    Revert imports to android.app.AlertDialog

--- app/src/main/java/com/nutomic/syncthingandroid/activities/FolderPickerActivity.java
+++ app/src/main/java/com/nutomic/syncthingandroid/activities/FolderPickerActivity.java
@@ -11,7 +11,6 @@ import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
-// import android.support.v7.app.AlertDialog;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
--- app/src/main/java/com/nutomic/syncthingandroid/activities/MainActivity.java
+++ app/src/main/java/com/nutomic/syncthingandroid/activities/MainActivity.java
@@ -2,7 +2,7 @@ package com.nutomic.syncthingandroid.activities;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.support.v7.app.AlertDialog;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.ComponentName;
 import android.content.DialogInterface;
