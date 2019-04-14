286ecd6f7640cf634cac4e1477c69439414a79ef
Catfriend1 <Catfriend1@users.noreply.github.com>
2019-04-14 15:09:50 +0200

    Remove unused import: TaskStackBuilder

--- app/src/main/java/com/nutomic/syncthingandroid/activities/SettingsActivity.java
+++ app/src/main/java/com/nutomic/syncthingandroid/activities/SettingsActivity.java
@@ -25,7 +25,6 @@ import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
