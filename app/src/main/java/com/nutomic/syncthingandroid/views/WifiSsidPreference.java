76a8a4af0d2c7d43572a3f35ffe49e365e99ee0e
Catfriend1 <Catfriend1@users.noreply.github.com>
2019-10-20 13:07:44 +0200

    Test on physical device under Android 9

--- app/src/main/java/com/nutomic/syncthingandroid/views/WifiSsidPreference.java
+++ app/src/main/java/com/nutomic/syncthingandroid/views/WifiSsidPreference.java
@@ -68,7 +68,7 @@ public class WifiSsidPreference extends MultiSelectListPreference {
     protected void showDialog(Bundle state) {
         SharedPreferences sharedPreferences = getSharedPreferences();
         Set<String> knownSsids;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q || true) {
             knownSsids = sharedPreferences.getStringSet(Constants.PREF_KNOWN_WIFI_SSIDS, new HashSet<>());
             String currentWifiSsid = getCurrentWifiSsid();
             if (!TextUtils.isEmpty(currentWifiSsid)) {
