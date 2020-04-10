60dd6ff2a9812e0444a0edbe0467faaaf15a9e5b
Catfriend1 <Catfriend1@users.noreply.github.com>
2020-04-10 22:22:03 +0200

    Revert "tmp - REVERT THIS"
    This reverts commit 4c212f7ed2f33bcc5791120309c20d5c9542c351.

--- app/src/main/java/com/nutomic/syncthingandroid/service/RestApi.java
+++ app/src/main/java/com/nutomic/syncthingandroid/service/RestApi.java
@@ -455,24 +455,7 @@ public class RestApi {
         synchronized (mConfigLock) {
             jsonConfig = mGson.toJson(mConfig);
         }
-        if (true) {
-            //Log.v(TAG, "sendConfig: config=" + (new javax.json.JsonObject(jsonConfig)).toString(spacesToIndentEachLevel));
-            String sb = jsonConfig;
-            if (sb.length() > 4000) {
-                Log.v(TAG, "sb.length = " + sb.length());
-                int chunkCount = sb.length() / 4000;     // integer division
-                for (int i = 0; i <= chunkCount; i++) {
-                    int max = 4000 * (i + 1);
-                    if (max >= sb.length()) {
-                        Log.v(TAG, "chunk " + i + " of " + chunkCount + ":" + sb.substring(4000 * i));
-                    } else {
-                        Log.v(TAG, "chunk " + i + " of " + chunkCount + ":" + sb.substring(4000 * i, max));
-                    }
-                }
-            } else {
-                Log.v(TAG, sb.toString());
-            }
-        }
+        // Log.v(TAG, "sendConfig: config=" + jsonConfig);
         new PostRequest(mContext, mUrl, PostRequest.URI_SYSTEM_CONFIG, mApiKey,
             null, jsonConfig, null);
         mOnConfigChangedListener.onConfigChanged();
