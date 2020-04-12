96ee8901d63486863fd2cd51cf3c87e675dc7a11
Catfriend1 <Catfriend1@users.noreply.github.com>
2020-04-12 19:42:13 +0200

    Update DevicesAdapter.java

--- app/src/main/java/com/nutomic/syncthingandroid/views/DevicesAdapter.java
+++ app/src/main/java/com/nutomic/syncthingandroid/views/DevicesAdapter.java
@@ -102,10 +102,10 @@ public class DevicesAdapter extends ArrayAdapter<Device> {
         final int completion = mRestApi.getRemoteDeviceCompletion(device.deviceID);
 
         if (conn.connected) {
-            String bandwidthUpDownText = "\u02c5 ";     // down arrow
+            String bandwidthUpDownText = "\u290a ";     // down arrow
             bandwidthUpDownText += Util.readableTransferRate(getContext(), conn.inBits);
             bandwidthUpDownText += " \u2022";           // dot
-            bandwidthUpDownText += " \u02c4 ";          // up arrow
+            bandwidthUpDownText += " \u2934 ";          // up arrow
             bandwidthUpDownText += Util.readableTransferRate(getContext(), conn.outBits);
             binding.bandwidthUpDown.setText(bandwidthUpDownText);
             rateInOutView.setVisibility(VISIBLE);
