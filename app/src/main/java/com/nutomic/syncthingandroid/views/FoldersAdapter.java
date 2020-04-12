0d9d0cf76e235cf72a271909f87605b6b1c2a62b
Catfriend1 <Catfriend1@users.noreply.github.com>
2020-04-12 15:26:14 +0200

    REVERT THIS: Tests

--- app/src/main/java/com/nutomic/syncthingandroid/views/FoldersAdapter.java
+++ app/src/main/java/com/nutomic/syncthingandroid/views/FoldersAdapter.java
@@ -219,7 +219,18 @@ public class FoldersAdapter extends ArrayAdapter<Folder> {
             default:
                 concat += " \u2049";
         }
-        concat += " " + Util.getPathEllipsis(cachedFolderStatus.lastItemFinishedItem, 44);
+        // concat += " " + Util.getPathEllipsis(cachedFolderStatus.lastItemFinishedItem);
+        Util.getPathEllipsis("");
+        Util.getPathEllipsis("jp2iexp.dll");
+        Util.getPathEllipsis("TP-LINK-/Easy Smart Configuration Utility/jre/bin/jp2iexp.dll");
+        Util.getPathEllipsis("TP-LINK-Ich-bin-ein-ASSHOLE-und-mach-lang/Easy Smart Configuration Utility/jre/bin/jp2iexp.dll");
+        Util.getPathEllipsis("TP-LINK-Ich-bin-ein-ASSHOLE-und-mach-lang-textdatei-ist-cool.txt");
+        Util.getPathEllipsis("TP-LINK-/Easy Smart Configuration Utility/jre/bin-Ich-bin-ein-ASSHOLE-und-mach-lang/jp2iexp-lang-textdatei-ist-cool.dll");
+        Util.getPathEllipsis("TP-LINK-//Easy Smart Configuration Utility/jre/bin-Ich-bin-ein-ASSHOLE-und-mach-lang/jp2iexp-lang-textdatei-ist-cool.dll");
+        Util.getPathEllipsis("TP-LINK-//Easy Smart Configuration Utility/jre/bin-Ich-bin-ein-ASSHOLE-und-mach-lang//jp2iexp-lang-textdatei-ist-cool.dll");
+        Util.getPathEllipsis("TP-LINK-//Easy Smart Configuration Utility/jre/bin-Ich-bin-ein-ASSHOLE-und-mach-lang//jp2iexp-lang-textdatei-ist-cool");
+        Util.getPathEllipsis("TP-LINK-//Easy Smart Configuration Utility/jre/bin-Ich-bin-ein-ASSHOLE-und-mach-lang//jp2iexp-lang-textdatei-ist-cool.richtiglangeextensionswassolldas-denn-bitte");
+        Util.getPathEllipsis("TP-LINK-Ich-bin-ein-ASSHOLE-und-mach-lang/Easy Smart Configuration Utility/jre/bin/jp2iexp.dllrichtiglangeextensionswassolldas-denn-bitte");
         return concat;
     }
 
