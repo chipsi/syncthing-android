ec768d8beb0c5490e177e561f0256490390e22d7
Catfriend1 <Catfriend1@users.noreply.github.com>
2020-04-12 18:16:43 +0200

    Update FoldersAdapter.java

--- app/src/main/java/com/nutomic/syncthingandroid/views/FoldersAdapter.java
+++ app/src/main/java/com/nutomic/syncthingandroid/views/FoldersAdapter.java
@@ -221,7 +221,9 @@ public class FoldersAdapter extends ArrayAdapter<Folder> {
         binding.lastItemFinishedItem.setText(finishedItemText);
         binding.lastItemFinishedItem.setVisibility(VISIBLE);
 
-        binding.lastItemFinishedTime.setText("\u21cc \u231a " + Util.formatTime(cachedFolderStatus.lastItemFinishedTime));
+        String finishedItemTime = "\u21cc \u231a ";
+        finishedItemTime += Util.formatTime(cachedFolderStatus.lastItemFinishedTime);
+        binding.lastItemFinishedTime.setText(finishedItemTime);
         binding.lastItemFinishedTime.setVisibility(VISIBLE);
 
         return;
