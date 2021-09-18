dec1fdf3d13b3f2dc9ae2577d821eb948eced703
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2021-09-18 09:52:33 +0200

    Create QuickSettingsTileCamera.java

---
+++ app/src/main/java/com/nutomic/syncthingandroid/service/QuickSettingsTileCamera.java
@@ -0,0 +1,25 @@
+package com.nutomic.syncthingandroid.service;
+
+import android.content.Intent;
+import android.os.Build;
+import android.service.quicksettings.TileService;
+
+import com.nutomic.syncthingandroid.activities.PhotoShootActivity;
+
+import androidx.annotation.RequiresApi;
+
+@RequiresApi(api = Build.VERSION_CODES.N)
+public class QuickSettingsTileCamera extends TileService {
+    public QuickSettingsTileCamera() {
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+    }
+
+    @Override
+    public void onClick() {
+        startActivity(new Intent(this, PhotoShootActivity.class));
+    }
+}
