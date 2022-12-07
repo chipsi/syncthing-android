fc4c9658ed615bd648720b0579f41a41a04154a7
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2022-12-08 00:46:05 +0100

    Update PermissionUtil.java

--- app/src/main/java/com/nutomic/syncthingandroid/util/PermissionUtil.java
+++ app/src/main/java/com/nutomic/syncthingandroid/util/PermissionUtil.java
@@ -3,6 +3,8 @@ package com.nutomic.syncthingandroid.util;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.Manifest;
+import android.os.Build;
+import android.os.Environment;
 // import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -13,6 +15,9 @@ public class PermissionUtil {
     // private static final String TAG = "PermissionUtil";
 
     public static boolean haveStoragePermission(@NonNull Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            return Environment.isExternalStorageManager();
+        }
         int permissionState = ContextCompat.checkSelfPermission(context,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE);
         return permissionState == PackageManager.PERMISSION_GRANTED;
