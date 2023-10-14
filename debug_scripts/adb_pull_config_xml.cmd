102c2aa4dae3308598ab1dfb9b431b7cab978c74
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2023-10-14 22:36:58 +0200

    Update adb_pull_config_xml.cmd

--- debug_scripts/adb_pull_config_xml.cmd
+++ debug_scripts/adb_pull_config_xml.cmd
@@ -8,9 +8,12 @@ REM
 REM 	SET PACKAGE_NAME=com.github.catfriend1.syncthingandroid
 SET PACKAGE_NAME=com.github.catfriend1.syncthingandroid.debug
 REM 
+REM 	SET DATA_ROOT=/data/user/0
+SET DATA_ROOT=/data/data
+REM 
 :loopMe
 cls
-adb shell "su root cat /data/data/%PACKAGE_NAME%/files/config.xml" > "%SCRIPT_PATH%config.xml"
+adb shell "su root cat %DATA_ROOT%/%PACKAGE_NAME%/files/config.xml" > "%SCRIPT_PATH%config.xml"
 IF EXIST "%SCRIPT_PATH%config.xml" TYPE "%SCRIPT_PATH%config.xml" | more
 echo.
 pause
