5fe821c7562099d5b5dc2bf257764ba489128ffd
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2024-10-21 21:32:29 +0200

    Update versions.gradle.kts

--- app/versions.gradle.kts
+++ app/versions.gradle.kts
@@ -1,6 +1,6 @@
 extra.apply {
     set("versionMajor", 1)
-    set("versionMinor", 27)
-    set("versionPatch", 12)
+    set("versionMinor", 28)
+    set("versionPatch", 0)
     set("versionWrapper", 0)
 }
