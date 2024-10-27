17cce8d1c5f90d741d286f51c08cee83642bfef4
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2024-10-27 09:37:03 +0100

    1

--- app/src/release/java/com/nutomic/syncthingandroid/service/FlavorConstants.java
+++ app/src/release/java/com/nutomic/syncthingandroid/service/FlavorConstants.java
@@ -2,7 +2,7 @@ package com.nutomic.syncthingandroid.service;
 
 import android.content.pm.ServiceInfo;
 
-public class FlavourConstants {
+public class FlavorConstants {
 
     public static final Integer FOREGROUND_SERVICE_TYPE         = ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE;
 
